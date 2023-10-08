e96bf458a6b70bcc436e210cf7ea6a300db0eaa5
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-10-08 14:02:29 -0300

    Adding structure files

---
+++ src/test/java/br/com/mybudget/javamybbackendusermanager/JavaMybBackendUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mybudget.javamybbackendusermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMybBackendUserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
