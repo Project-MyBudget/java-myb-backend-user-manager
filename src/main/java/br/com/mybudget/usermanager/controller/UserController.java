43aae112dd4c389e01ae3866351455bceafbc968
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-10-13 02:54:03 -0300

    Creating user GET resource

--- src/main/java/br/com/mybudget/usermanager/controller/UserController.java
+++ src/main/java/br/com/mybudget/usermanager/controller/UserController.java
@@ -1,9 +1,12 @@
 package br.com.mybudget.usermanager.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mybudget.usermanager.model.dto.ResponseStatusLogDTO;
 import br.com.mybudget.usermanager.model.dto.UserDTO;
+import br.com.mybudget.usermanager.model.entity.UserEntity;
 import br.com.mybudget.usermanager.service.UserService;
 
 @RestController
@@ -32,4 +36,21 @@ public class UserController {
 		return userService.registerUser(requestRegisterUser);
 	}
 	
+	/**
+	 * Tries to retrive a user from the database's application.
+	 * 
+	 * @author Marcos Vinicius
+	 * @param PathVariable long id
+	 * @return ResponseEntity<UserEntity>
+	 */
+	@GetMapping(value = "/user/id/{id}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<UserEntity> getUserById(@PathVariable long id) {
+		UserEntity response = userService.findByIdUser(id);
+		
+		if (response != null) {
+			return ResponseEntity.ok(response);
+		}
+		return ResponseEntity.notFound().build();
+		//TODO: Implement Optional<UserEntity>.
+	}
 }
